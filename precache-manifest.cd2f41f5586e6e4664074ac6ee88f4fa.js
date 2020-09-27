f2b49d24d732861c82f26e654f1862d6b2df6e24
wagdy magdy <58633699+wagdy2019@users.noreply.github.com>
2020-09-27 05:59:40 +0200

    Updates

---
+++ precache-manifest.cd2f41f5586e6e4664074ac6ee88f4fa.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ea032e4a8fd8fd74c161c076268a7bd",
+    "url": "/shopReact/index.html"
+  },
+  {
+    "revision": "9bf87296cf0b0f25481d",
+    "url": "/shopReact/static/css/2.f52fb0be.chunk.css"
+  },
+  {
+    "revision": "39dc9ae2e9a5e59ee06c",
+    "url": "/shopReact/static/css/main.d74dca84.chunk.css"
+  },
+  {
+    "revision": "9bf87296cf0b0f25481d",
+    "url": "/shopReact/static/js/2.df8a982f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/shopReact/static/js/2.df8a982f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39dc9ae2e9a5e59ee06c",
+    "url": "/shopReact/static/js/main.65d2f1d8.chunk.js"
+  },
+  {
+    "revision": "07981e9243bba36f51df",
+    "url": "/shopReact/static/js/runtime-main.5b1873f0.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/shopReact/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/shopReact/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/shopReact/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/shopReact/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/shopReact/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
